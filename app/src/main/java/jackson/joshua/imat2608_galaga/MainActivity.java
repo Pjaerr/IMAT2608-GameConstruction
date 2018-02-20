b73f4f90eff3aa53bda1ecfa6d1f858673f839d8
Joshua Jackson <washingtonfaulconer@gmail.com>
2018-02-20 12:48:48 +0000

    Initial Project Folder Commit

---
+++ app/src/main/java/jackson/joshua/imat2608_galaga/MainActivity.java
@@ -0,0 +1,13 @@
+package jackson.joshua.imat2608_galaga;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
